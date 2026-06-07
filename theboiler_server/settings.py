821b13e6ae355094f3d6e54da77dd367c78dce44
Chani17 <cucute8878@naver.com>
2026-06-07 11:03:24 +0900

    Update: secret key .env로 분리

--- theboiler_server/settings.py
+++ theboiler_server/settings.py
@@ -1,17 +1,19 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d@ya_uw9nuvru&=e8n#qjmn&)6w2q^%6jksl7hesmycj^!d=xm"
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
